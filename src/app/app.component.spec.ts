6e9d31edfe1815d03e706a78838eec1cc0b9e3a8
irisdecembre <thanhtrang181220@gmail.com>
2022-08-24 00:15:01 +0700

    Update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyCoin'`, () => {
+  it(`should have as title 'ChanCoin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyCoin');
+    expect(app.title).toEqual('ChanCoin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyCoin app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ChanCoin app is running!');
   });
 });
